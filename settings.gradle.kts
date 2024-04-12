18a8b505df675d4c7226ca6ec51f422cd63d8236
KilldenHI <killpochta@gmail.com>
2024-04-12 04:51:26 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ru.mirea.Zherebtsov.Lesson5"
+include(":app")
+include(":accelerometer")
+include(":camera1")
+include(":audiorecord")
